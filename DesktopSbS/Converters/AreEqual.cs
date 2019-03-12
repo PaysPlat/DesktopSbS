b5d0ace2e94953cce5a222b8d5db2f35609e68fd
PaysPlat <paysplat@gmail.com>
2019-03-12 01:24:27 +0100

    Converters

---
+++ DesktopSbS/Converters/AreEqual.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Data;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Markup;
+using System.ComponentModel;
+
+namespace DesktopSbS.Converters
+{
+
+    [MarkupExtensionReturnType(typeof(IMultiValueConverter))]
+    public class AreEqual : MarkupExtension, IMultiValueConverter
+    {
+        #region IValueConverter Members
+
+
+        public object Convert(object[] values, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (values == null) return Binding.DoNothing;
+
+            bool isEqual = true;
+
+            for (int i = 1; i < values.Length; ++i)
+            {
+                isEqual &= values[i - 1]?.Equals(values[i]) == true;
+                if (!isEqual) break;
+            }
+            return isEqual;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+
+        private static IMultiValueConverter converter = null;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (converter == null)
+            {
+                converter = new AreEqual();
+            }
+            return converter;
+
+
+        }
+    }
+}
---
+++ DesktopSbS/Converters/DoubleOperation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+using System.Linq;
+
+namespace DesktopSbS.Converters
+{
+    [MarkupExtensionReturnType(typeof(IMultiValueConverter))]
+    [ValueConversion(typeof(double), typeof(double))]
+    public class DoubleOperation : MarkupExtension, IMultiValueConverter
+    {
+        #region IMultiValueConverter Members
+
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null) return Binding.DoNothing;
+
+            List<double> operands = values.Select(v => v is double ? v : (v is int ? (double?)(int)v : null) ).OfType<double>().ToList();
+
+            if (operands.Count == 0) return Binding.DoNothing;
+
+            if (operands.Count == 1) return operands[0];
+
+            string op = (parameter as string)?.ToLower();
+            switch (op)
+            {
+                case "add":
+                default:
+                    return operands.Aggregate((a, b) => a + b);
+                case "substract":
+                    return operands.Aggregate((a, b) => a - b);
+                case "multiply":
+                    return operands.Aggregate((a, b) => a * b);
+                case "divide":
+                    return operands.Aggregate((a, b) => a / b);
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private static IMultiValueConverter converter = null;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (converter == null)
+            {
+                converter = new DoubleOperation();
+            }
+            return converter;
+
+
+        }
+
+    }
+}
